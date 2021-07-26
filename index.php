13bdc362ad00e1f855bd24e600119638f57fdec5
Christian Banzali <banzalichristian1@gmail.com>
2021-07-26 12:30:54 +0800

    added whatwedo

--- index.php
+++ index.php
@@ -1,54 +1,77 @@
 <!DOCTYPE html>
+
 <head>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <title>C.V. Solutions Inc.</title>
     <link type="text/css" rel="stylesheet" href="index.css">
 </head>
+
 <body>
-<!--navbar-->
-<div class="container">
-<div class="nav-bar">
+    <!--navbar-->
+    <div class="container">
+        <div class="nav-bar">
 
-<img src="images/cvlogo.png" style="height:50px; width:180px;">
-<ul class="nav-links">
-    <li><a href="">Services</a></li>
-    <li><a href="">Organization</a></li>
-    <li><a href="">Career</a></li>
-    <li><a href="">About Us</a></li>
-    <li><a href="">Contact Us</a></li>   
-</ul>
-<div class="burger">
-<div class="line"></div>
-<div class="line"></div>
-<div class="line"></div>
-</div>
-</div>
-<!--cover photo -->
-<div class="coverphoto"></div>
+            <img src="images/cvlogo.png" style="height:50px; width:180px;">
+            <ul class="nav-links">
+                <li><a href="">Services</a></li>
+                <li><a href="">Organization</a></li>
+                <li><a href="">Career</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+            </ul>
+            <div class="burger">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+        </div>
+        <!--cover photo -->
+        <div class="coverphoto"></div>
 
-<!--whycv?-->
-<div class="whycv">
-<div>
-<h1>WHY C.V. SOLUTIONS?</h1>
-<p>Despite being a relatively young company, CVSI personnel have the knowledge, expertise and experience.
-was established and duly registered with the Securities and Exchange Commission on 26 April 2001 as a 100% Filipino-owned company involved in the manufacture and marketing of Specialty Chemical Programs and Services, and Specialty Projects and Materials to various industries.</p>
-</div>
-<div>
-<video class="dnavideo" controls>
-					<source src="videos/dna.mp4" type="video/mp4">
-				</video>
-</div>
-</div>
-<div class="whatwedo">
-<p class="why">What We do & <span class="why1"> We Do It Well</span></p>
-</div>
+        <!--whycv?-->
+        <div class="whycv">
+            <div>
+                <h1>WHY C.V. SOLUTIONS?</h1>
+                <p>Despite being a relatively young company, CVSI personnel have the knowledge, expertise and experience.
+                    was established and duly registered with the Securities and Exchange Commission on 26 April 2001 as a 100% Filipino-owned company involved in the manufacture and marketing of Specialty Chemical Programs and Services, and Specialty Projects and Materials to various industries.</p>
+            </div>
+            <div>
+                <video class="dnavideo" controls>
+                    <source src="videos/dna.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+        <div class="whatwedo">
+            <p class="whatwedo-title">What We do & We Do It Well</span></p>
+            <div class="whatwedo-con">
+                <div class="whatwedoo">
+                    <img src="images/monitor1.png">
+                    <p>Safety Standards <br><br>
+                    <span>designed to ensure the safety of products, activities and processes, etc. They may be advisory or compulsory and are normally laid down by an advisory or regulatory body that may be either voluntary or statutory.</span>
+                    </p>
+                </div>
+                <div class="whatwedoo">
+                    <img src="images/monitor2.png">
+                    <p>Product Quality <br><br>
+                    <span>the standard of something as measured against other things of a similar kind; the degree of excellence of something.</span>
+                    </p>
+                </div>
+                <div class="whatwedoo">
+                    <img src="images/monitor3.png">
+                    <p>Privacy Policy <br><br>
+                    <span>This Privacy Policy is meant to help you understand what information we collect, why we collect it, and how you can update, manage, export, and delete your information.</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
 
 
-</div>
-<script src="index.js"></script> 
+
+    <script src="index.js"></script>
 </body>
-</html>
 
+</html>
