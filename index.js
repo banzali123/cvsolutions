adb2ff669e1af9b75cd6de15209c45caea906d6a
Christian Banzali <banzalichristian1@gmail.com>
2021-07-26 15:37:36 +0800

    added slick carousel for customers

--- index.js
+++ index.js
@@ -12,3 +12,42 @@ burger.addEventListener('click' , () => {
 
 
 navSlide();
+
+
+
+$('.trusted-images').slick({
+
+    autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings:  {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 
 <head>
+    <link rel="shortcut icon" href="pictures/example1.png" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <title>C.V. Solutions Inc.</title>
     <link type="text/css" rel="stylesheet" href="index.css">
+
+    <!--jquery-->
+    <link rel="stylesheet" type="text/css" href="slick.css">
+	<link rel="stylesheet" type="text/css" href="slick-theme.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script  src="slick.min.js"></script>
+    
 </head>
 
 <body>
@@ -41,53 +49,105 @@
                 </video>
             </div>
         </div>
+
+        <!--whatwedo-->
         <div class="whatwedo">
             <p class="whatwedo-title">What We do & We Do It Well</span></p>
             <div class="whatwedo-con">
                 <div class="whatwedoo">
                     <img src="images/monitor1.png">
                     <p>Safety Standards <br><br>
-                    <span>designed to ensure the safety of products, activities and processes, etc. They may be advisory or compulsory and are normally laid down by an advisory or regulatory body that may be either voluntary or statutory.</span>
+                        <span>designed to ensure the safety of products, activities and processes, etc. They may be advisory or compulsory and are normally laid down by an advisory or regulatory body that may be either voluntary or statutory.</span>
                     </p>
                 </div>
                 <div class="whatwedoo">
                     <img src="images/monitor2.png">
                     <p>Product Quality <br><br>
-                    <span>the standard of something as measured against other things of a similar kind; the degree of excellence of something.</span>
+                        <span>the standard of something as measured against other things of a similar kind; the degree of excellence of something.</span>
                     </p>
                 </div>
                 <div class="whatwedoo">
                     <img src="images/monitor3.png">
                     <p>Privacy Policy <br><br>
-                    <span>This Privacy Policy is meant to help you understand what information we collect, why we collect it, and how you can update, manage, export, and delete your information.</span>
+                        <span>This Privacy Policy is meant to help you understand what information we collect, why we collect it, and how you can update, manage, export, and delete your information.</span>
                     </p>
                 </div>
             </div>
         </div>
+        <!--events-->
+        <div class="events">
+            <div class="a"><img src="images/corporate1.png"></div>
+            <div class="b"><img src="images/corporate2.png"></div>
+            <div class="c"><img src="images/corporate3.png"></div>
+        </div>
 
-    <div class="events">
-    <div class="a"><img src="images/corporate1.png"></div>  
-    <div class="b"><img src="images/corporate2.png"></div>
-    <div class="c"><img src="images/corporate3.png"></div>
-    </div>
+        <!--headlines-->
+        <div class="headlines">
+            <h1>HEADLINES</h1>
+            <ul>
+                <li>
+                    <p>MAY 12<br><a href="">2021 - Automation Anywhere Imagine</a> </span></p>
+                </li>
+                <li>
+                    <p>MAY 14<br><a href="">Limited Board Declares Quarterly Cash Dividend</a></span></p>
+                </li>
+                <li>
+                    <p>MAY 21<br><a href="">Fast Company Partner to Digitally Transform a World Changing Ideas Honoree</a></span></p>
+                </li>
+                <li>
+                    <p>JUNE 06<br><a href="">2021 - Automation Anywhere Imagine</a></span></p>
+                </li>
+                <li>
+                    <p>JUNE 12<br><a href="">2021 - Automation Anywhere Imagine</a></span></p>
+                </li>
+            </ul>
+        </div>
 
-    <div class="headlines">
-    <h1>HEADLINES</h1>
-    <ul>
-    <li><p>MAY 12<br><a href="">2021 - Automation Anywhere Imagine</a> </span></p></li>
-    <li><p>MAY 14<br><a href="">Limited Board Declares Quarterly Cash Dividend</a></span></p></li>
-    <li><p>MAY 21<br><a href="">Fast Company Partner to Digitally Transform a World Changing Ideas Honoree</a></span></p></li>        
-    <li><p>JUNE 06<br><a href="">2021 - Automation Anywhere Imagine</a></span></p></li>
-    <li><p>JUNE 12<br><a href="">2021 - Automation Anywhere Imagine</a></span></p></li>
-    </ul>
-
-    </div>
-
-
+        <div class="trustedcustomer">
+        <h1>Trusted Customer</h1>
+        <div class="trusted-images">
+			<img src="images/partners/linde.png" alt="linde">
+			<img src="images/partners/delmonte.png" alt="partners">
+			<img src="images/partners/dole.png" alt="dole">
+			<img src="images/partners/alsons.png" alt="alsons">
+			<img src="images/partners/metro.png" alt="metro">
+			<img src="images/partners/mdh.png" alt="mdh">
+			<img src="images/partners/ladeco.png" alt="ladeco">
+			<img src="images/partners/gea.png" alt="gea">
+			<img src="images/partners/afi.png" alt="afi">
+			<img src="images/partners/banana.png" alt="banana">
+			<img src="images/partners/awpi.png" alt="awpi">
+			<img src="images/partners/amt.png" alt="amt">
+			<img src="images/partners/chemlux.png" alt="chemlux">
+			<img src="images/partners/davao.png" alt="davao">
+			<img src="images/partners/dizon.png" alt="dizon">
+			<img src="images/partners/tuna.png" alt="tuna">
+			<img src="images/partners/jx.png" alt="jx">
+			<img src="images/partners/marbco.png" alt="marbco">
+			<img src="images/partners/lapanday.png" alt="lapanday">
+			<img src="images/partners/marsman.png" alt="marsman">
+			<img src="images/partners/nakashin.png" alt="nakashin">
+			<img src="images/partners/nutrimeal.png" alt="nutrimeal">
+			<img src="images/partners/medcore.png" alt="medcore">
+			<img src="images/partners/osmont.png" alt="osmont">
+			<img src="images/partners/pasar.png" alt="pasar">
+			<img src="images/partners/rejan.png" alt="rejan">
+			<img src="images/partners/safro.png" alt="safro">
+			<img src="images/partners/silangan.png" alt="silangan">
+			<img src="images/partners/sumifru.png" alt="sumifru">
+			<img src="images/partners/sgae.png" alt="sgae">
+			<img src="images/partners/swiss.png" alt="swiss">
+			<img src="images/partners/treelife.png" alt="treelife">
+			<img src="images/partners/univanich.png" alt="univanich">
+		</div>
+        </div>
 
 
     </div>
     <script src="index.js"></script>
+
+
+
 </body>
 
 </html>
